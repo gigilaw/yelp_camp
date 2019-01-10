96f6c567dd0b642b45a1c9090930777a86616035
gigilaw <gigi.law.14@gmail.com>
2019-01-10 18:01:12 +0800

    styles

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose.connect(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set(`view engine`, `ejs`);
-
+app.use(express.static(__dirname + '/public'));
 seedDB();
 
 app.get('/', function(req, res) {
--- seeds.js
+++ seeds.js
@@ -7,18 +7,21 @@ let data = [
     name: 'Clouds Hill',
     image:
       'https://www.quebecoriginal.com/en/listing/images/800x600/c3491525-6325-4ff1-b4fc-02333721c0cb/camping-fort-ramsay-sites-sur-le-bord-de-leau.jpg',
-    description: 'Clouds. Lots of clouds.'
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vestibulum dui, ac dapibus dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce sed arcu vehicula, elementum augue et, placerat nulla. Fusce convallis orci consequat nisi hendrerit rutrum. Sed laoreet velit ac massa posuere porttitor. Nunc laoreet tempor lorem, sed aliquam eros venenatis at. Cras mauris augue, gravida non pulvinar ac, ullamcorper quis odio. Maecenas malesuada elit nisl, id elementum urna varius sed. Nulla fringilla nisi eu vulputate tristique. Aliquam bibendum neque sit amet euismod semper. Vivamus nec nisi placerat, sollicitudin metus posuere, dapibus metus. Ut vitae mi a nunc hendrerit imperdiet quis nec arcu. Vivamus eu gravida dolor, nec imperdiet enim. Quisque bibendum, ipsum at efficitur pellentesque, purus mi pellentesque felis, id tristique nunc nisl quis nisi.'
   },
   {
     name: 'Sunshine Meadow',
     image:
       'https://www.idahocampgroundreview.com/images/campgrounds/sunnygulch/sun_fac/sunnygulchgroup.JPG',
-    description: 'Sunshine. Lots of sunshine.'
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vestibulum dui, ac dapibus dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce sed arcu vehicula, elementum augue et, placerat nulla. Fusce convallis orci consequat nisi hendrerit rutrum. Sed laoreet velit ac massa posuere porttitor. Nunc laoreet tempor lorem, sed aliquam eros venenatis at. Cras mauris augue, gravida non pulvinar ac, ullamcorper quis odio. Maecenas malesuada elit nisl, id elementum urna varius sed. Nulla fringilla nisi eu vulputate tristique. Aliquam bibendum neque sit amet euismod semper. Vivamus nec nisi placerat, sollicitudin metus posuere, dapibus metus. Ut vitae mi a nunc hendrerit imperdiet quis nec arcu. Vivamus eu gravida dolor, nec imperdiet enim. Quisque bibendum, ipsum at efficitur pellentesque, purus mi pellentesque felis, id tristique nunc nisl quis nisi.'
   },
   {
     name: 'Sunset Range',
     image: 'https://www.laketullochcampground.com/index_htm_files/3578@2x.jpg',
-    description: 'Sunset. Beautiful sunset.'
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vestibulum dui, ac dapibus dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce sed arcu vehicula, elementum augue et, placerat nulla. Fusce convallis orci consequat nisi hendrerit rutrum. Sed laoreet velit ac massa posuere porttitor. Nunc laoreet tempor lorem, sed aliquam eros venenatis at. Cras mauris augue, gravida non pulvinar ac, ullamcorper quis odio. Maecenas malesuada elit nisl, id elementum urna varius sed. Nulla fringilla nisi eu vulputate tristique. Aliquam bibendum neque sit amet euismod semper. Vivamus nec nisi placerat, sollicitudin metus posuere, dapibus metus. Ut vitae mi a nunc hendrerit imperdiet quis nec arcu. Vivamus eu gravida dolor, nec imperdiet enim. Quisque bibendum, ipsum at efficitur pellentesque, purus mi pellentesque felis, id tristique nunc nisl quis nisi.'
   }
 ];
 
