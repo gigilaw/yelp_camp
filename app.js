d552d46a4ed3a3551ab4f2e44e40d9587f3158ce
gigilaw <gigi.law.14@gmail.com>
2019-01-16 14:18:03 +0800

    Login setup

--- app.js
+++ app.js
@@ -131,6 +131,21 @@ app.post('/register', function(req, res) {
   });
 });
 
+//Login Routes
+//Login form
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+//login logic
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login'
+  }),
+  function(req, res) {}
+);
+
 app.listen(3000, () => {
   console.log('Server Started');
 });
