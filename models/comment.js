79753bd31055cd63325d2790e294dc48c9aac786
gigilaw <gigi.law.14@gmail.com>
2019-02-01 18:35:25 +0800

    moment js

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,7 @@
 let mongoose = require('mongoose');
 let commentSchema = mongoose.Schema({
   text: String,
+  createdAt: { type: Date, default: Date.now },
   author: {
     id: {
       type: mongoose.Schema.Types.ObjectId,
